830f303f727786bc95f699c054f53cc54f07ff65
Ghulam Mustafa <mustafaji@gmail.com>
2022-01-25 19:35:36 +0100

    Added jwtAuthentication

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,46 +1,50 @@
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../utils/jwtAuthentication.js";
 
-import { generateAccessToken, generateRefreshToken } from '../utils/jwtAuthentication.js';
-
-const users = [{
-    id: '1',
-    email: 'codefor',
-    password: '1234'
-}];
+const users = [
+  {
+    id: "1",
+    email: "codefor",
+    password: "1234",
+  },
+];
 
 let tokens = [];
 
 export const userLogin = (request, response) => {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const user = users.find(user => user.email === email && user.password === password);
-    
-    if (user) {
-        const accessToken = generateAccessToken(user);
-        const refreshToken = generateRefreshToken(user);
+  const user = users.find(
+    (user) => user.email === email && user.password === password
+  );
 
-        tokens.push(refreshToken);
+  if (user) {
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
 
-        response.json({
-            email: user.email,
-            accessToken,
-            refreshToken
-        })
+    tokens.push(refreshToken);
 
-    } else {
-        response.status(400).json("Incorrect username or password");
-    }
-}
+    response.json({
+      email: user.email,
+      accessToken,
+      refreshToken,
+    });
+  } else {
+    response.status(400).json("Incorrect username or password");
+  }
+};
 
 export const getUser = (request, response) => {
-    if(request.user.id === request.params.id) {
-        console.log('//// ', request.user);
-    } else {
-
-    }
-}
+  if (request.user.id === request.params.id) {
+    console.log("//// ", request.user);
+  } else {
+  }
+};
 
 export const logoutUser = (request, response) => {
-    const refreshToken = request.body.token;
-    tokens = tokens.filter(token => token !== refreshToken);
-    response.status(200).json('Logged out successfully');
-}
+  const refreshToken = request.body.token;
+  tokens = tokens.filter((token) => token !== refreshToken);
+  response.status(200).json("Logged out successfully");
+};
